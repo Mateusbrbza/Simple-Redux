1f5d3af802a25c52c515c998aba6a080c391c4cc
Mateus <105020728+Mateusbrbza@users.noreply.github.com>
2022-08-17 00:26:11 -0300

    Add files via upload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,31 @@
 import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store/counter-slice';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 });
-  };
+    dispatch(counterActions.increase(5)); 
+  }; // {type: some_unique_identifier, payload: xx}
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   const decreaseHandler = () => {
-    dispatch({ type: 'decrease', amount: 5 });
+    dispatch(counterActions.decrease(5));
   };
 
   const toggleCounterHandler = () => {
-    dispatch ({ type: 'toggleCounter' });
+    dispatch (counterActions.toggleCounter());
   };
 
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Header.module.css';
+import { authActions } from '../store/Auth-slice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>Meus Produtos</a>
-          </li>
-          <li>
-            <a href='/'>Minhas Vendas</a>
-          </li>
-          <li>
-            <button>Sair</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>Meus Produtos</a>
+            </li>
+            <li>
+              <a href='/'>Minhas Vendas</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Sair</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
